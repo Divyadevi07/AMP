6ecb41e50d9bc83c9bafbe4641ff09aadb0dc205
divyadevi07 <divyadevil@sciera.com>
2023-07-29 12:36:29 +0530

    SSO login added

---
+++ src/app/_config/app.config.ts
@@ -0,0 +1,3 @@
+export const allowedGmailDomain = ['sciera.com'];
+
+export const googleClientId = ['817890910358-8hug3f3f7o7gugm0cj4jp3d0d74l4a9a.apps.googleusercontent.com'];
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { googleClientId, allowedGmailDomain } from '../_config/app.config';
+import jwt_decode from "jwt-decode";
+import * as $ from "jquery";
+declare let google: any;
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit {
 
+  constructor(private router: Router) { }
+
+  ngAfterViewInit(): void {
+
+    setTimeout(() => {
+      // console.log(google)
+      google.accounts.id.initialize({
+        client_id: googleClientId,
+        callback: this.handleCredentialResponse.bind(this),
+      });
+      google.accounts.id.renderButton(
+        document.getElementById('google-btn'),
+        { theme: 'outline', size: 'large' } // customization attributes
+      );
+      google.accounts.id.prompt(); // also display the One Tap dialog
+    }, 1000)
+
+  }
+
+  handleCredentialResponse(res: any) {
+    const loginDetails: any = jwt_decode(res.credential);
+    const selectedCredentials: any = {
+      email: loginDetails.email,
+      email_verified: loginDetails.email_verified,
+      picture: loginDetails.picture,
+      last_name: loginDetails.family_name,
+      googleid: loginDetails.jti,
+      first_name: loginDetails.given_name
+    }
+    if (loginDetails.hd && allowedGmailDomain.indexOf(loginDetails.hd) > -1) {
+      this.router.navigateByUrl('/template');
+      localStorage.setItem('template', JSON.stringify(selectedCredentials))
+      $('#hidden-btn').click();
+    }
+    else {
+      $('.text-danger').css('display', 'block');
+      setTimeout(() => {
+        $('.text-danger').css('display', 'none');
+      }, 3000);
+    }
+  }
 }
